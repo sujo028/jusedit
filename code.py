5d9fcec01470fe4335eef9253e69131adf560e5c
sujo028 <48432120+sujo028@users.noreply.github.com>
2019-10-24 22:56:21 +0530

    Code.java
    Print operation java

--- code.py
+++ code.py
@@ -1,2 +1,8 @@
 print"bleh bleh"
 print"done"
+public class main{
+    public static void main(String[] args) {
+        System.out.println("blend bleh");
+        System.out.println("done");
+   }
+}
